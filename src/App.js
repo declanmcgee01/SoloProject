ef125d28f76de3dfcc41d4c9df5b434ae0bee65d
Declan McGee <declanmcgee1995@gmail.com>
2019-01-22 15:59:25 +0000

    First Commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './NavBar';
+import SearchBar from './SearchBar';
 
 class App extends Component {
   render() {
@@ -10,7 +11,30 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <NavBar/>
+          <h1 id = "Home-Page-Header">
+            Norse Gods 
+          </h1> 
+          <body>
+            <input type="text" placeholder="Search Gods.." name="search" id = "Search-Bar"></input>
+            <p id="God-Information">
+            Name: Odin
+            <br></br>
+            <br></br>
+            Other Names: Allfather
+            <br></br>
+            <br></br>
+            God Of: Wisdompoetry, War, Death, Divination and Magic
+            <br></br>
+            <br></br>
+            Children: Thor, Baldr, Víðarr and Váli
+            <br></br>
+            <br></br>
+            Description: Son of Bor and the giantess (jötunn) Bestla, Odin is the chief of the Æsir and the king of Asgard.
+            </p>
+          </body>
         </header>
+        <body>
+        </body>
       </div>
     );
   }
---
+++ src/SearchBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './SearchBar.css';
+
+
+class SearchBar extends Component {
+    render(){
+        return(
+            <body>
+            </body>
+        );
+    }
+}
+export default SearchBar;
